7078b155fe7c5ba1df9cf89dd186588d1a7f2613
Cooper Miller <kcoopermiller9@gmail.com>
2024-04-30 14:56:37 -0400

    fix voicevox flake

--- voicevox.nix
+++ voicevox.nix
@@ -1,8 +1,14 @@
-{ lib, stdenv, fetchurl, glib, glibc, nss, autoPatchelfHook, gtk3
+{ system ? builtins.currentSystem, pkgs ? import <nixpkgs> { inherit system; }, lib, stdenv, fetchurl, autoPatchelfHook, ffmpeg, glib, glibc, nss, gtk3
 , nspr, cups, dbus, libdrm, pango, cairo, xorg, mesa, expat, libxkbcommon
 , alsa-lib, at-spi2-atk, portaudio, gcc_debug
 }:
-
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/50a7139fbd1acd4a3d4cfa695e694c529dd26f3a.tar.gz";
+    sha256="1rh75qfcdbczm2rdzqni21xj0wc8f92mhnpwq5mv3z0yy8f35krl";
+  }) { inherit system; };
+  onnxruntime_1_13_1 = pkgs.onnxruntime;
+in
 stdenv.mkDerivation rec {
   pname = "voicevox";
   version = "0.18.1";
@@ -17,6 +23,7 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    # ffmpeg
     glib
     glibc
     nss
@@ -41,15 +48,27 @@ stdenv.mkDerivation rec {
     at-spi2-atk
     portaudio
     gcc_debug
+    onnxruntime_1_13_1
     stdenv.cc.cc.lib
   ];
-  
 
   installPhase = ''
-    ls $src
     runHook preInstall
-    install -m755 -D $src $out/bin/voicevox
-    runHook postInstall
+
+    mkdir -p $out/bin
+    mkdir -p $out/lib
+
+    tar -xzvf $src
+
+    extracted_dir=$(find . -maxdepth 1 -type d -name "VOICEVOX")
+
+    cp -r $extracted_dir/* $out/bin/
+    rm $out/bin/lib*
+    # rm $out/bin/vv-engine/lib* 
+    cp $extracted_dir/lib* $out/lib/
+    cp -r $extracted_dir/vv-engine/lib* $out/lib/
+
+    runHook postInstall  
   '';
 
   meta = with lib; {
